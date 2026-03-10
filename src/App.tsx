8e647cc49c6f6d92821860d5108499a5c7969a9d
Zvi Moshe <zvi293293@gmail.com>
2026-03-10 18:59:43 +0200

    Fix admin SVG preview safety and enable router future flags

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
         <CustomCursor />
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
